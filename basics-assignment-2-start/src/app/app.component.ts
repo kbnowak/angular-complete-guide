1e6ac2c9218026d8b7af340bd331698c637a3b99
Krzysztof Nowak <krzysztof.b.nowak@gmail.com>
2018-06-16 11:09:57 +0200

    assignment 2 solution

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  usernameEmpty = true;
+  username = '';
+
+  usernameChanged() {
+    this.usernameEmpty = this.username.length === 0;
+  }
+
+  resetUsername() {
+    this.username = '';
+    this.usernameEmpty = true;
+  }
 }
