140adf430006ad39b7262b687c5275a3c37be5fe
Krzysztof Nowak <krzysztof.b.nowak@gmail.com>
2018-06-19 22:43:59 +0200

    improved ngFor

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   showParagraph = false;
   buttonClicks = [];
-  buttonClicksCount = 1;
+  // buttonClicksCount = 1;
   toggleParagraph() {
     this.showParagraph = !this.showParagraph;
-    this.buttonClicks.push(this.buttonClicksCount++);
+    // this.buttonClicks.push(this.buttonClicksCount++);
+    this.buttonClicks.push(new Date());
   }
 }
