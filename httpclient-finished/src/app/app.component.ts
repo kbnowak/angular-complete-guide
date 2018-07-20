1a9ec2b8fc1d1cfb528fe5ad675d2e22888d1cd1
Krzysztof Nowak <krzysztof.b.nowak@gmail.com>
2018-07-20 22:01:02 +0200

    changed firebase project

--- httpclient-finished/src/app/app.component.ts
+++ httpclient-finished/src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyBrkKleAX_8jHpPmTchVBmDD7Hkj8TT1VE",
-      authDomain: "ng-recipe-book-3adbb.firebaseapp.com"
+      apiKey: "AIzaSyCTDQKZVgNxViOpZ-pa3OWgz7WNcP4R3Fw",
+      authDomain: "angular-complete-guide-5e55a.firebaseapp.com"
     });
   }
 
--- httpclient-finished/src/app/shared/data-storage.service.ts
+++ httpclient-finished/src/app/shared/data-storage.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
 import 'rxjs/Rx';
 
-import { RecipeService } from '../recipes/recipe.service';
-import { Recipe } from '../recipes/recipe.model';
-import { AuthService } from '../auth/auth.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
@@ -21,13 +21,13 @@ export class DataStorageService {
     //   params: new HttpParams().set('auth', token)
     //   // headers: headers
     // });
-    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    const req = new HttpRequest('PUT', 'https://angular-complete-guide-5e55a.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
     return this.httpClient.request(req);
   }
 
   getRecipes() {
     // this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+    this.httpClient.get<Recipe[]>('https://angular-complete-guide-5e55a.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
